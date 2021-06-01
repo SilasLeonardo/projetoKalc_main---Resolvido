d2deb008c24f835fc5bab556d8bdb74c97731a39
Silas Leonardo <silas.leonardo15@gmail.com>
2021-06-01 05:22:16 -0300

    Primeira parte do projeto pronto!

---
+++ js/kalcWeb.js
@@ -0,0 +1,78 @@
+console.log("To funcionando!!!");
+
+// Operacoes
+const divisao = document.getElementById("divisao");
+const multiplicacao = document.getElementById("multiplicacao");
+const subtracao = document.getElementById("subtracao");
+const soma = document.getElementById("soma");
+const igual = document.getElementById("igual");
+
+// Numeros
+const nove = document.getElementById("nove")
+const oito = document.getElementById("oito")
+const sete = document.getElementById("sete")
+const seis = document.getElementById("seis")
+const cinco = document.getElementById("cinco")
+const quatro = document.getElementById("quatro")
+const tres = document.getElementById("tres")
+const dois = document.getElementById("dois")
+const um = document.getElementById("um")
+const clear = document.getElementById("clear")
+const ponto = document.getElementById("ponto")
+const zero = document.getElementById("zero")
+
+// Eventos Operações
+divisao.onclick = function() {
+    console.log(divisao.innerText);
+}
+multiplicacao.onclick = function() {
+    console.log(multiplicacao.innerText);
+}
+subtracao.onclick = function() {
+    console.log(subtracao.innerText);
+}
+soma.onclick = function() {
+    console.log(soma.innerText);
+}
+igual.onclick = function() {
+    console.log(igual.innerText);
+}
+
+
+// Eventos numeros
+nove.onclick = function() {
+    console.log(nove.innerText);
+}
+oito.onclick = function() {
+    console.log(oito.innerText);
+}
+sete.onclick = function() {
+    console.log(sete.innerText);
+}
+seis.onclick = function() {
+    console.log(seis.innerText);
+}
+cinco.onclick = function() {
+    console.log(cinco.innerText);
+}
+quatro.onclick = function() {
+    console.log(quatro.innerText);
+}
+tres.onclick = function() {
+    console.log(tres.innerText);
+}
+dois.onclick = function() {
+    console.log(dois.innerText);
+}
+um.onclick = function() {
+    console.log(um.innerText);
+}
+clear.onclick = function() {
+    console.log(clear.innerText);
+}
+ponto.onclick = function() {
+    console.log(ponto.innerText);
+}
+zero.onclick = function() {
+    console.log(zero.innerText);
+}
